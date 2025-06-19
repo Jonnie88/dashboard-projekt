85c7e39baf38d645eb2c9eaed08088bb6dcaa68a
Johannes <Johanneslundberg88@gmail.com>
2025-06-19 10:21:47 +0200

    Lagt till Tailwind CSS och konfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  base: "/dashboard-projekt/",
+  plugins: [svelte(), tailwindcss()],
+});
